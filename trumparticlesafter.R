bfafdc28e5a1ed6e35212bdaa8fc9e893dceb4aa
Cecelia Kaufmann <ceceiekaufmann@gmail.com>
2023-03-23 11:04:55 -0500

    updated on biden and trump NTY before and after

--- trumparticlesafter.R
+++ trumparticlesafter.R
@@ -2,19 +2,19 @@ library(jsonlite)
 library(tidyverse)
 library(lubridate)
 
-key <- "&api-key=UkLuFruKUXYehFSXy0sgNGaFwPkB6HEa"
-url <- "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=biden&being_date=20200101&end_date=20200101&page=1"
+key <- "&api-key=9ta4cAbFQANV7eEtSnxC66sXFtDNHF3W"
+url <- "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=trump&being_date=20210101&end_date=20220101&page=1"
 req <- fromJSON(paste0(url, key))
 articles <- req$response$docs
-link = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=biden"
+link = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=trump"
 
 # url <- param_set(link, "api-key", url_encode(key))
 
 
-dates <- ymd('20200101') + 0:500
+dates <- ymd('20210101') + 0:500
 d <- format(dates,'%Y%m%d')
 
-bidenarticles <- NULL
+trumparticlesafter <- NULL
 
 
 for(i in d){
@@ -23,7 +23,7 @@ for(i in d){
     url = paste0(link, '&begin_date=',i ,'&end_date=',i ,'&page=',p)
     req <- fromJSON(paste0(url, key))
     articles <- req$response$docs
-    bidenarticles <- bind_rows(bidenarticles,articles)
+    trumparticlesafter <- bind_rows(trumparticlesafter,articles)
     if(isTRUE(nrow(articles)) && nrow(articles) != 10){ break }
     else{p = p+1}
     Sys.sleep(6)
@@ -31,8 +31,4 @@ for(i in d){
 }
 
 
-save(bidenarticles, file = "bidenarticles.RData")
-
-
-
-
+save(trumparticlesafter, file = "trumparticlesafter.RData")
