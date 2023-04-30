8bb33513a6b097ffbc60085850bb849bb4f4f1c9
yiyangshi-hub <yshi@macalester.edu>
2023-04-30 16:45:47 -0500

    simplify the pull requests

--- Pull Requests/guardian.R
+++ Pull Requests/guardian.R
@@ -1,13 +1,11 @@
-
-
 key <- "&api-key=395b850e-d5c2-4414-aed5-02beddcbbd23"
 url <- "https://content.guardianapis.com/search?q=trump&from-date=2020-01-01&to-date=2020-01-01&page=1"
 req <- fromJSON(paste0(url, key))
 articles <- req$response$results
-link = "https://content.guardianapis.com/search?q=trump"
+link = "https://content.guardianapis.com/search?q=trump" # you can adjust the keyword after q
 
 
-dates <- ymd('20200101') + 0:365
+dates <- ymd('20200101') + 0:365 # you can adjust the time range for the pull request
 d <- format(dates,'%Y-%m-%d')
 
 totalarticles <- NULL
@@ -38,7 +36,7 @@ for (i in 1:10) {
   article <- read_html(totalarticles$webUrl[i])
  cssSelector <- 
     article %>% 
-    html_elements(".article-body-viewer-selector") %>% #.dcr-n6w1lc, .dcr-az7egx, .dcr-1up63on, .dcr-8zipgp, .dcr-94xsh, .dcr-1gesh1i") %>% 
+    html_elements(".article-body-viewer-selector") %>% 
     html_attr('class') %>% str_split(' ') %>% sapply(tail, n=1) %>% paste0('.',.)
     if(cssSelector =='.'){body_text  = NULL}
     else{
@@ -49,8 +47,8 @@ for (i in 1:10) {
   body_text_tot <- bind_rows(body_text_tot, body_text_coll)
 }
 
-# save(body_text_tot, file = "trump_guardian_text.RData")
 
+# here are the CSS selectors if you decide to webscrape by them
+# .dcr-n6w1lc, .dcr-az7egx, .dcr-1up63on, .dcr-8zipgp, .dcr-94xsh, .dcr-1gesh1i
 
-# html_element(".dcr-az7egx", ".dcr-1up63on", ".dcr-8zipgp", ".dcr-94xsh", "dcr-1gesh1i")
 
